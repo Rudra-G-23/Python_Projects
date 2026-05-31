25ad0fd2666eaf1681daf268c5bc52f48488c0fd
Rudra Prasad Bhuyan <rudraprasadbhuyan999@gmail.com>
2026-05-31 17:23:13 +0530

    2026 Feb work

---
+++ 2026/test/text.py
@@ -0,0 +1,40 @@
+""" 
+LAYER 1 : Concept Sheet
+One page Per Model
+
+For each Formula
+    - Formula
+    - Assumptions
+    - Hyperparameters
+    - When to use
+    - When NOT to use
+    - Real-world example
+"""
+
+""" 
+LAYER 2 : Implementation Layer
+
+For each Formula
+    - Implement from sklearn
+    - Then Code on small data
+"""
+
+""" 
+LAYER 3 : Interview Simulation
+Ask Yourself:
+    - Explain Random Forest to a 12-year-old
+    - How loss function work in DL
+"""
+
+""" 
+QUESTIONS: 
+- What type of problem does it solve?
+- What is its mathematical formula?
+- What is the loss function?
+- How does optimization happen?
+- What assumptions does it make?
+- What causes overfitting?
+- What parameters control complexity?
+- When should I NOT use this model?
+
+"""
